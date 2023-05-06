480784c4a83c3042f2efa3056485cd868a237446
NguyenMinhQuan123 <nguyenminhquan20602@gmail.com>
2023-05-07 00:30:03 +0700

    àasf
    kạhfáhf

--- app/src/main/java/com/manhhdc/qltk/SecondFragment.java
+++ app/src/main/java/com/manhhdc/qltk/SecondFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.manhhdc.qltk.databinding.FragmentSecondBinding;
+
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
